9f64fcb9010d83f92f4a3ae4788dc96a3d2a9157
kde-rooi <kde-rooi@student.codam.nl>
2021-11-08 14:50:37 +0100

    scraps on cpp08

--- cpp08/ex00/includes/easyfind.hpp
+++ cpp08/ex00/includes/easyfind.hpp
@@ -1,6 +1,12 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
-template <typename T> T easyfind( T, int );
+template <typename T, int n>
+T& easyfind( T<n> container, int instance ){
+	for ( int i = 0; container[i]; i++ )
+		if ( container[i] == instance )
+			return container[i];
+	throw e
+}
 
 #endif
