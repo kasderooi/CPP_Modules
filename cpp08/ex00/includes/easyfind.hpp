d3a1c9f3edb19e0cc4015b00e66f3af88ea1136a
Kas <kasderooij@gmail.com>
2021-11-09 07:03:23 +0100

    new approach cpp08/ex00

--- cpp08/ex00/includes/easyfind.hpp
+++ cpp08/ex00/includes/easyfind.hpp
@@ -1,12 +1,23 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
-template <typename T, int n>
-T& easyfind( T<n> container, int instance ){
-	for ( int i = 0; container[i]; i++ )
-		if ( container[i] == instance )
-			return container[i];
-	throw e
+#include <iostream>
+#include <iterator>
+
+template <typename T>
+T& easyfind( T container, int instance ){
+	
+	for( typename T::iterator it = container.begin(); it < container.end(); it++ ){
+		if ( (*it) == instance )
+			return (*it);
+	}
+	return NULL;
+}
+
+template <typename T>
+std::ostream & operator<<( std::ostream & o, T const & element ){
+	o << (*element);
+	return o;
 }
 
 #endif
--- cpp08/ex00/srcs/main.cpp
+++ cpp08/ex00/srcs/main.cpp
@@ -1,5 +1,12 @@
 #include "easyfind.hpp"
+#include <vector>
 
 int main ( void ){
+	std::vector<int> ar{ 1, 2, 3, 4, 5, 6, 7 };
+	int			a = 3;
+	int			b = 8;
 
+	std::cout << easyfind( ar, a ) << std::endl;
+	std::cout << easyfind( ar, b ) << std::endl;
+	return 0;
 }
